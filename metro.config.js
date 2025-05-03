a36c41773bb81832079d978f50aa4a9772f93081
Alberto <alberto.moreno2357@gmail.com>
2025-05-03 11:46:19 +0200

    fix: app working again

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,8 @@
-// metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-config.resolver.unstable_conditionNames = ["require"];
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (moduleName === "axios") {
-    return context.resolveRequest(
-      { ...context, unstable_conditionNames: ["browser", "default", "require"] },
-      moduleName,
-      platform
-    );
-  }
-  return context.resolveRequest(context, moduleName, platform);
-};
+config.resolver.unstable_enablePackageExports = false;
 
 module.exports = config;
